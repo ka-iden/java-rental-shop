1e2d6a8113b4a5a2acd29594b1818e2e4c340111
Kaiden B. <kaidenchrisbird@gmail.com>
2025-04-13 17:09:12 +1000

    Make all buttons open hello alert

--- src/main/java/io/github/it_is_final/java_rental_shop/Controller.java
+++ src/main/java/io/github/it_is_final/java_rental_shop/Controller.java
@@ -1,19 +1,20 @@
 package io.github.it_is_final.java_rental_shop;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         String wText = String.format(
-                "Welcome to JavaFX %s! Running on Java %s.",
-                System.getProperty("javafx.version"),
-                System.getProperty("java.version")
-        );
-        welcomeText.setText(wText);
+            "Welcome to JavaFX %s! Running on Java %s.",
+            System.getProperty("javafx.version"),
+            System.getProperty("java.version")
+         );
+        Alert alert = new Alert(AlertType.INFORMATION, wText, ButtonType.OK);
+        alert.showAndWait();
     }
 }
