53399f638fd8bb0f905a9bac57b3e76c68ac71dd
Luong Truong <s4170302@student.rmit.edu.au>
2025-04-14 23:08:04 +1000

    Change data structure of StoreInventory.videos to Map
    This simplifies getting and searching videos, no longer needing an implementation of binary search.

--- src/main/java/io/github/it_is_final/java_rental_shop/StoreInventory.java
+++ src/main/java/io/github/it_is_final/java_rental_shop/StoreInventory.java
@@ -3,48 +3,25 @@ package io.github.it_is_final.java_rental_shop;
 import java.util.*;
 
 public class StoreInventory {
-    private final List<Video> videos;
+    private final Map<String, Video> videos;
 
     public StoreInventory() {
-        videos = new ArrayList<>();
+        videos = new HashMap<>();
     }
 
     public void addVideo(Video video) {
-        videos.add(video);
-        // Make binary search easier
-        videos.sort(Comparator.comparing(Video::name));
+        videos.put(video.name(), video);
     }
 
-    public List<Video> searchVideosByTitle(String q) {
+    public void getVideoByTitle(String q) {
+        videos.get(q);
+    }
+
+    public List<Video> searchVideos(String q) {
         return videos
+                .values()
                 .stream()
                 .filter((v) -> v.name().contains(q))
                 .toList();
-    }
-
-    public List<Video> searchVideosByAuthor(String q) {
-        return videos
-                .stream()
-                .filter((v) -> v.author().contains(q))
-                .toList();
-    }
-
-    public Video getVideoByTitle(String t) {
-        int l = 0;
-        int r = videos.size() - 1;
-        // Binary search implementation
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (videos.get(m).name().compareTo(t) < 0) {
-                l = m + 1;
-            }
-            if (videos.get(m).name().compareTo(t) > 0) {
-                r = m - 1;
-            }
-            if (videos.get(m).name().compareTo(t) == 0) {
-                return videos.get(m);
-            }
-        }
-        return null;
     }
 }
